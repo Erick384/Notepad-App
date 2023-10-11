c8957f087bf2f4280b919117ef4ff9cfae99d023
Erick384 <erickwachuma@gmail.com>
2023-10-11 17:53:07 +0300

    Feat: Create && Fetch data/ Appwrite

--- notes-app-client/src/App.jsx
+++ notes-app-client/src/App.jsx
@@ -1,8 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { Client, Databases, ID } from "appwrite";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 function App() {
   // State variables
+  const [error, setError] = useState("");
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [selectedNote, setSelectedNote] = useState(null);
@@ -15,19 +19,53 @@ function App() {
     // ... other notes ...
   ]);
 
-  // Function to add a new note
-  const handleAddNote = (event) => {
-    event.preventDefault();
-    const newNote = {
-      id: notes.length + 1,
-      title: title,
-      content: content,
-    };
-    setNotes([newNote, ...notes]);
-    setTitle("");
-    setContent("");
-  };
+  // APPWRIRE============================================
+  const client = new Client()
+    .setEndpoint("https://cloud.appwrite.io/v1")
+    .setProject("65268afae6d40bb318c6");
 
+  const databases = new Databases(client);
+  // ===============================================
+  // Function to add a new note
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+  const handleAddNote = async (event) => {
+    event.preventDefault();
+    try {
+      const newNote = {
+        title: title,
+        content: content,
+      };
+      // setNotes([newNote, ...notes]);
+      const response = await databases.createDocument(
+        "65268b448e885e57bd2f",
+        "65268b8b248f48b848c9",
+        ID.unique(),
+        newNote
+      );
+      console.log(response);
+      toast("Added successfully");
+      setTitle("");
+      setContent("");
+    } catch (error) {
+      console.error(error.message);
+      setError(error.message);
+      toast(error.message);
+    }
+  };
+  const fetchNotes = async () => {
+    try {
+      const {documents} = await databases.listDocuments(
+        "65268b448e885e57bd2f",
+        "65268b8b248f48b848c9"
+      );
+      console.log(documents);
+      setNotes(documents);
+    } catch (error) {
+      toast(error.message);
+    }
+  };
   // Function to handle a click on a note
   const handleNoteClick = (note) => {
     setSelectedNote(note);
@@ -66,18 +104,16 @@ function App() {
     setSelectedNote(null);
   };
 
-
   // Function to delete a note
-  const deleteNote = function(event, noteId) {
+  const deleteNote = function (event, noteId) {
     event.stopPropagation();
-  
-    const updatedNotes = notes.filter(function(note) {
+
+    const updatedNotes = notes.filter(function (note) {
       return note.id !== noteId;
     });
-  
+
     setNotes(updatedNotes);
   };
-  
 
   return (
     <div className="app-container">
@@ -111,10 +147,10 @@ function App() {
         )}
       </form>
       <div className="notes-grid">
-        {notes.map((note) => (
+        {notes && notes.map((note,index) => (
           <div
             className="note-item"
-            key={note.id}
+            key={index}
             onClick={() => handleNoteClick(note)}
           >
             <div className="notes-header">
@@ -125,6 +161,7 @@ function App() {
           </div>
         ))}
       </div>
+      <ToastContainer />
     </div>
   );
 }
--- notes-app-server/src/index.ts
+++ notes-app-server/src/index.ts
@@ -1,15 +1,76 @@
-import express from "express";
-import cors from "cors";
+// import express from "express";
+// import cors from "cors";
 
-const app = express();
+// const app = express();
 
-app.use(express.json());
-app.use(cors());
+// app.use(express.json());
+// app.use(cors());
 
-app.get("/api/notes", async (req, res) => {
-  res.json({ message: "success!" });
+// app.get("/api/notes", async (req, res) => {
+//   res.json({ message: "success!" });
+// });
+
+// app.listen(5000, () => {
+//   console.log("server running on localhost:5000");
+// });
+
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
+import expressAsyncHandler from "express-async-handler";
+
+const dbPromise = open({
+  filename: "./database.db",
+  driver: sqlite3.Database,
 });
 
-app.listen(5000, () => {
-  console.log("server running on localhost:5000");
-});
+interface Note {
+  id: number;
+  title: string;
+  content: string;
+}
+
+// Function to insert a new note
+export const addNote = async (
+  title: string,
+  content: string
+): Promise<number> => {
+  const db = await dbPromise;
+  const result = await db.run(
+    "INSERT INTO notes (title, content) VALUES (?, ?)",
+    [title, content]
+  );
+  return result.lastID as number;
+};
+
+// Function to retrieve all notes
+export const getAllNotes = async (): Promise<Note[]> => {
+  const db = await dbPromise;
+  return db.all("SELECT * FROM notes");
+};
+
+// Function to retrieve a single note by ID
+export const getNoteById = async (id: number): Promise<Note | undefined> => {
+  const db = await dbPromise;
+  return db.get("SELECT * FROM notes WHERE id = ?", [id]);
+};
+
+// Function to update a note by ID
+export const updateNote = async (
+  id: number,
+  title: string,
+  content: string
+): Promise<number> => {
+  const db = await dbPromise;
+  const result = await db.run(
+    "UPDATE notes SET title = ?, content = ? WHERE id = ?",
+    [title, content, id]
+  );
+  return result.changes as number;
+};
+
+// Function to delete a note by ID
+export const deleteNote = async (id: number): Promise<number> => {
+  const db = await dbPromise;
+  const result = await db.run("DELETE FROM notes WHERE id = ?", [id]);
+  return result.changes as number;
+};
