931385c04c32ef38d959004f7837bed976becaf9
Erick384 <erickwachuma@gmail.com>
2023-10-12 11:56:15 +0300

    Feat: Update Notes

--- notes-app-client/src/App.jsx
+++ notes-app-client/src/App.jsx
@@ -5,6 +5,7 @@ import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 function App() {
+
   // State variables
   const [error, setError] = useState("");
   const [title, setTitle] = useState("");
@@ -16,7 +17,6 @@ function App() {
       title: "test note 1",
       content: "bla bla note1",
     },
-    // ... other notes ...
   ]);
 
   // APPWRIRE============================================
@@ -51,21 +51,40 @@ function App() {
     } catch (error) {
       console.error(error.message);
       setError(error.message);
-      toast(error.message);
+      toast.error( error.message, {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+        });
     }
   };
   const fetchNotes = async () => {
     try {
-      const {documents} = await databases.listDocuments(
+      const { documents } = await databases.listDocuments(
         "65268b448e885e57bd2f",
         "65268b8b248f48b848c9"
       );
       console.log(documents);
       setNotes(documents);
     } catch (error) {
-      toast(error.message);
+      toast.error( error.message, {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+        });
     }
   };
+
   // Function to handle a click on a note
   const handleNoteClick = (note) => {
     setSelectedNote(note);
@@ -74,7 +93,30 @@ function App() {
   };
 
   // Function to update a note
-  const handleUpdateNote = (event) => {
+  // const handleUpdateNote = (event) => {
+  //   event.preventDefault();
+
+  //   if (!selectedNote) {
+  //     return;
+  //   }
+
+  //   const updatedNote = {
+  //     id: selectedNote.id,
+  //     title: title,
+  //     content: content,
+  //   };
+
+  //   const updatedNotesList = notes.map((note) =>
+  //     note.id === selectedNote.id ? updatedNote : note
+  //   );
+
+  //   setNotes(updatedNotesList);
+  //   setTitle("");
+  //   setContent("");
+  //   setSelectedNote(null);
+  // };
+
+  const handleUpdateNote = async (event) => {
     event.preventDefault();
 
     if (!selectedNote) {
@@ -82,19 +124,41 @@ function App() {
     }
 
     const updatedNote = {
-      id: selectedNote.id,
       title: title,
       content: content,
     };
 
-    const updatedNotesList = notes.map((note) =>
-      note.id === selectedNote.id ? updatedNote : note
-    );
+    try {
+      const response = await databases.updateDocument(
+        "65268b448e885e57bd2f",
+        "65268b8b248f48b848c9",
+        selectedNote.id,
+        updatedNote
+      );
 
-    setNotes(updatedNotesList);
-    setTitle("");
-    setContent("");
-    setSelectedNote(null);
+      const updatedNotesList = notes.map((note) =>
+        note.id === selectedNote.id ? { ...note, ...updatedNote } : note
+      );
+
+      setNotes(updatedNotesList);
+      setTitle("");
+      setContent("");
+      setSelectedNote(null);
+      toast("Updated successfully");
+    } catch (error) {
+      console.error(error.message);
+      setError(error.message);
+      toast.error( error.message, {
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+        });
+    }
   };
 
   // Function to cancel note editing
@@ -147,19 +211,22 @@ function App() {
         )}
       </form>
       <div className="notes-grid">
-        {notes && notes.map((note,index) => (
-          <div
-            className="note-item"
-            key={index}
-            onClick={() => handleNoteClick(note)}
-          >
-            <div className="notes-header">
-              <button onClick={(event) => deleteNote(event, note.id)}>x</button>
+        {notes &&
+          notes.map((note, index) => (
+            <div
+              className="note-item"
+              key={index}
+              onClick={() => handleNoteClick(note)}
+            >
+              <div className="notes-header">
+                <button onClick={(event) => deleteNote(event, note.id)}>
+                  x
+                </button>
+              </div>
+              <h2>{note.title}</h2>
+              <p>{note.content}</p>
             </div>
-            <h2>{note.title}</h2>
-            <p>{note.content}</p>
-          </div>
-        ))}
+          ))}
       </div>
       <ToastContainer />
     </div>
